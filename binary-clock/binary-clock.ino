bc44e889084c974603e03cf3ac9d612bf5fda83e
koos42 <koos42@gmail.com>
2020-02-03 11:06:53 -0800

    refactor binary clock to allow for dimming of the LEDs by my own hand-rolled PWM

--- binary-clock/binary-clock.ino
+++ binary-clock/binary-clock.ino
@@ -2,6 +2,16 @@ const unsigned int outputPins = 0x07FF; // 0b 0000 0111 1111 1111
 const unsigned int secondPins = 0x0001; // 0b 0000 0000 0000 0001
 const unsigned int minutePins = 0x007E; // 0b 0000 0000 0111 1110
 const unsigned int hourPins   = 0x0780; // 0b 0000 0111 1000 0000
+const unsigned int tick = 10; // in ms.
+const unsigned int ticksPerSec = 1000 / tick;
+
+unsigned int seconds;
+unsigned int tickCount;
+float brightness; // a percent
+
+unsigned int outputPinState;
+
+// #define DEBUG
 
 void setup() {  
   #ifdef DEBUG
@@ -11,19 +21,29 @@ void setup() {
   for (int i = 0; i < 11; i++) {
     pinMode(i, OUTPUT);
   }
+
+  seconds = 0;
+  outputPinState = 0;
+  tickCount = 0;
+  brightness = 0.2;
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  int seconds = 0;
-  
-  // at 12 hours' worth of seconds, allow loop() to complete and restart.
-  while (seconds < 43200) {
-    seconds += 1;
-    unsigned int outputPinState = setOutputPinState(seconds);
-    displayOutputPinState(outputPinState);
-    delay(1000);
+  // ensure that tickCount loops, and seconds tick on the second
+  if (tickCount >= ticksPerSec) {
+    tickCount = 0;
+    seconds++;
   }
+  
+  // ensure this is a 12 hour clock
+  if (seconds >= 43200) {
+    seconds = 0;
+  }
+
+  outputPinState = setOutputPinState(seconds);
+  setDisplay();
+  
+  tickCount++;
 }
 
 unsigned int setOutputPinState(int seconds) {
@@ -70,7 +90,23 @@ unsigned int computeHoursMask(int seconds) {
   return mask | ~hourPins;
 }
 
-void displayOutputPinState(unsigned int outputPinState) {  
+void setDisplay() {
+  // use pulse width modulation via delay to adjust brightness
+  int onPulseWidth = tick * brightness;
+  int offPulseWidth = tick - onPulseWidth;
+  
+  displayNothingViaPins();
+  delay(offPulseWidth);
+
+  displayOutputViaPins();
+  delay(onPulseWidth);
+
+  #ifdef DEBUG
+  displayOutputViaSerial();
+  #endif
+}
+
+void displayOutputViaPins() {
   for (int i = 0; i < 11; i++) {
     if (bitRead(outputPinState, i)) {
       digitalWrite(i, HIGH);
@@ -78,14 +114,17 @@ void displayOutputPinState(unsigned int outputPinState) {
       digitalWrite(i, LOW);
     }
   }
-
-  #ifdef DEBUG
-  displayOutputPinStateSerial(outputPinState);
-  #endif
 }
 
+void displayNothingViaPins() {
+  for (int i = 0; i < 11; i++) {
+    digitalWrite(i, LOW);
+  }
+}
+
+
 #ifdef DEBUG
-void displayOutputPinStateSerial(unsigned int outputPinState) {
+void displayOutputViaSerial() {
   Serial.print("pins: 0  1  2  3  4  5  6  7  8  9  10\n");
   Serial.print("      ");
   
